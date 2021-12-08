4f0289c1542f408d9f9b5ded80e13d3949ebd799
Luc Modolo <l.modolo@rafisa.ch>
2021-12-08 20:17:32 +0100

    Revert "Panel erstellt"
    This reverts commit 372c269f516d63015f7d987dd76288f84ede6c74.

--- src/App.js
+++ src/App.js
@@ -4,27 +4,24 @@ import Panel from "./Panel.js"
 
 import './App.css';
 
-class App extends Component {
-  constructor(props) {
-    super (props);
-
-        </div>
-        <br />
-        <Panel title Ueberschrift="Überschrift" contend label="Test1234567" />
-        <br />
-        <Panel title title Ueberschrift="Noch eine Überschrift" contend label="Test123456" />
-        </div>     
-      
-    );
-  }
+class App extends Component {  
 
 render(){
   return (
-    <div className="App">
-      <h2>Hallo {this.state.name}!</h2>
-      <p>{this.state.name.length}</p>
-    </div>
-  )
+      <div className="App">
+        <div className="App-header">
+            <h2 callName="App-heading">"Welcome to React" </h2>
+       
+        </div> 
+        <br /> 
+          <Panel title="Überschrift" content="Test1234567" /> 
+          <br/>
+
+          <Panel title="Noch eine Überschrift" content="Der Content">
+          <button Label= "Test" />
+          </Panel>
+      </div>  
+        )
  }
 }
 
